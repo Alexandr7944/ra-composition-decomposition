a84b62b25b32ab58e0c5742374e555ecd1bd7d43
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2023-06-01 10:48:04 +0300

    initial commit

---
+++ src/components/NewsList/NewsList.tsx
@@ -0,0 +1,25 @@
+import dataNew1 from '../../data/dataNew1.json'
+import NewsItem from '../NewsItem/NewsItem';
+
+/**
+ * Делается запрос на сервер по переданному URL 
+ * получает список новостей и поштучно передает их в NewsItem
+ */
+
+type NewsListProps = {
+  url: string
+}
+
+const NewsList: React.FC<NewsListProps> = ({ url }) => {
+  const newsList = url;
+
+  return (
+    <ul className='news-list'>
+      {
+        dataNew1.map(item => <NewsItem news={item} key={item.id}/>)
+      }
+    </ul>
+  )
+}
+
+export default NewsList
